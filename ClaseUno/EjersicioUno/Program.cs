5e2e83098189e2a0b144862650e093c11d48c55a
marcos514 <marmarreyer@gmail.com>
2017-08-16 11:40:56 -0300

    Clase Uno Max Min Toma de Var

---
+++ ClaseUno/EjersicioUno/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjersicioUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir 5 numeros, mayor y menor
+            int numeroMayor=0;
+            int numeroMenor=0;
+            int numero;
+            int contador;
+
+            for (contador = 0; contador < 5; contador++)
+            {
+                Console.Write("Ingrese un numero: ");
+                while(!int.TryParse(Console.ReadLine(),out numero))
+                {
+                    Console.Clear();
+                    Console.Write("Error. Ingrese un numero: ");
+
+                }
+
+                if (contador == 0)
+                {
+                    numeroMayor=numero;
+                    numeroMenor=numero;
+                }
+                else
+                {
+                    if (numero > numeroMayor)
+                    {
+                        numeroMayor=numero;
+                    }
+                    else if(numero<numeroMenor)
+                    {
+                        numeroMenor=numero;
+                    }
+                }
+                
+            }
+            Console.Clear();
+            Console.WriteLine("El numero maximo es: " + numeroMayor);
+            Console.WriteLine("El numero minimo es: " + numeroMenor);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ ClaseUno/PrimeraAplicacion/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumeroUno;
+            int NumeroDos;
+            int resultado;
+            string Dato;
+            bool esNumero;
+
+
+
+
+            Console.Write("Ingrese un numero: ");
+            while (!int.TryParse(Console.ReadLine(), out NumeroUno))
+            {
+                Console.Clear();
+                Console.Write("Error. Ingrese un numero: ");
+            }
+
+
+            Console.Clear();
+
+            Console.Write("Ingrese otro numero: ");
+            while (!int.TryParse(Console.ReadLine(), out NumeroDos))
+            {
+                Console.Clear();
+                Console.Write("Error. Ingrese un numero: ");
+                
+            }
+
+
+            resultado = NumeroUno + NumeroDos;
+            Console.Clear();
+            Console.WriteLine("Numero Uno es = " + NumeroUno + "\nNumero Dos es = " + NumeroDos + "\nEl resultado es = " + resultado);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
